6fabcf2b6398ecee529806c904eaa8246bff75e2
Reese <reesehe.66146@gmail.com>
2025-04-06 02:02:00 +0100

    Created flappy bird project.

---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Program {
+    public class Program {
+        public static void Main() { // Main program where event loop runs
+            Console.WriteLine("Flappy Bird program loading...");
+            // Initialises vairables
+            int ScreenX = 640;
+            int ScreenY = 360;
+            // Initialises assets and objects
+
+            // Initialises Raylib stuff
+            Raylib.InitWindow(ScreenX, ScreenY, "Flappy Bird");
+            Raylib.SetTargetFPS(120);
+            Console.WriteLine("All program components loaded.");
+            while (Raylib.WindowShouldClose()) {
+                //if
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# This is a flappy bird game
+
+# Bird class
+ - Rect
+ - Moves method, if recieves input then increases height, else goes down, should work like a parabola but the x position doesn't change
+
+# Pipe class
+ - Creates rect, heights are passed in
+ - Move method which moves it across the screen and reset back when goes off
+ - Allows height to be changed
+ - As game plays, distance can be decreased between the pipes
+
+# Main
+ - Loads in Bird and Pipes
+ - Constantly looks for input for bird position
+ - Updates display with all new pos
